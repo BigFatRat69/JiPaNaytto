ddc088ea5efee693812ef1d04783af8a75f768e2
Eetu.E <eetueskelinen123@gmail.com>
2026-03-26 12:27:56 +0200

    Add listing contacts

--- frontend/src/components/Employees/Employee.jsx
+++ frontend/src/components/Employees/Employee.jsx
@@ -4,7 +4,14 @@ export default function Employee({ emp }) {
 			<img src={emp.imageUrl} width="150" />
 			<h3>{emp.name}</h3>
 			<h2>{emp.role}</h2>
-			<h2>{emp.contacts}</h2>
+
+			{emp.contacts.map((contact, index) => (
+				<div key={index}>
+					<div>{contact.email}</div>
+					<div>{contact.phone}</div>
+				</div>
+			))}
+
 			<div>{emp.description}</div>
 		</>
 	);
