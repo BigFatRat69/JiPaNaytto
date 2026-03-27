ca2eec8e5c9f9cb50e176eed1313a8898507f829
Joel H. <143587522+Joelh123@users.noreply.github.com>
2026-03-27 13:00:14 +0200

    use bootstrap in displaying employees

--- frontend/src/components/Employees/Employee.jsx
+++ frontend/src/components/Employees/Employee.jsx
@@ -1,12 +1,28 @@
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+
 export default function Employee({ emp }) {
 	return (
 		<>
-			<img src={emp.imageUrl} width="150" />
+			<Card style={{ width: "18rem" }}>
+				<Card.Img variant="top" src={emp.imageUrl} width="150" />
+				<Card.Body>
+					<Card.Title>{emp.name}</Card.Title>
+					<Card.Text>{emp.description}</Card.Text>
+				</Card.Body>
+				<ListGroup className="list-group-flush">
+					<ListGroup.Item>{emp.role}</ListGroup.Item>
+					<ListGroup.Item>{emp.contacts[0].phone}</ListGroup.Item>
+					<ListGroup.Item>{emp.contacts[0].email}</ListGroup.Item>
+				</ListGroup>
+			</Card>
+
+			{/* <img src={emp.imageUrl} width="150" />
 			<h3>{emp.name}</h3>
 			<h2>{emp.role}</h2>
 			<div>{emp.contacts[0].email}</div>
 			<div>{emp.contacts[0].phone}</div>
-			<div>{emp.description}</div>
+			<div>{emp.description}</div> */}
 		</>
 	);
 }
