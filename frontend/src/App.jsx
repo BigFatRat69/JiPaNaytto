415856f487b0b5f163dff0b498ae39153a19768f
Eetu.E <eetueskelinen123@gmail.com>
2026-03-30 10:34:07 +0300

    Improve look of navbar. Improvements for homePage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,22 +4,34 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import { Routes, Route, Link } from "react-router-dom";
 
+import { Navbar, Nav, Container } from "react-bootstrap";
+
 export default function App() {
 	return (
 		<>
-			<nav>
-				<Link to="/">Etusivu</Link>
-				<Link to="/employees">Henkilöt</Link>
-				<Link to="/projects">Projektit</Link>
-				<Link to="/contact">Yhteystiedot</Link>
-			</nav>
+			<Navbar bg="dark" variant="dark" expand="lg">
+				<Container>
+					<Navbar.Brand as={Link} to="/">Yritys</Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="me-auto">
+							<Nav.Link as={Link} to="/">Etusivu</Nav.Link>
+							<Nav.Link as={Link} to="/employees">Henkilöt</Nav.Link>
+							<Nav.Link as={Link} to="/projects">Projektit</Nav.Link>
+							<Nav.Link as={Link} to="/contact">Yhteystiedot</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
 
-			<Routes>
-				<Route path="/" element={<HomePage />} />
-				<Route path="/employees" element={<Employees />} />
-				<Route path="/projects" element={<Projects />} />
-				<Route path="/contact" element={<Contact />} />
-			</Routes>
+			<Container className="mt-4">
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/employees" element={<Employees />} />
+					<Route path="/projects" element={<Projects />} />
+					<Route path="/contact" element={<Contact />} />
+				</Routes>
+			</Container>
 		</>
 	);
 }
--- frontend/src/components/homePage.jsx
+++ frontend/src/components/homePage.jsx
@@ -1,7 +1,44 @@
+import { Container, Card } from "react-bootstrap";
+
 export default function HomePage() {
 	return (
-		<>
-			<h1>Jyväskylän Insinööripalvelu Oy</h1>
-		</>
+		<Container className="mt-4">
+			<Card className="p-4 shadow">
+				<Card.Body>
+					<Card.Title as="h1" className="mb-3">
+						Jyväskylän Insinööripalvelu Oy
+					</Card.Title>
+
+					<Card.Text>
+						Jipa eli Jyväskylän Insinööripalvelu Oy on vuonna 2025 
+						perustettu uusi ketterä insinööritoimisto. Jipa keskittyy 
+						yrityksen alkutaipaleella tarjoamaan asiakkailleen uudis- 
+						sekä korjauskohteiden rakennus- ja rakennesuunnittelua, 
+						sekä valvontaa ja rakennuttamista.
+					</Card.Text>
+
+					<Card.Text>
+						Tavoite on saada asiakkaille kokonaispalvelua rakentamisen 
+						konsultoinnin osalta ja täten keventää tilaajapuolen työtä 
+						hankkeissa.
+					</Card.Text>
+				</Card.Body>
+			</Card>
+			<Card className="p-4 shadow">
+								<Card.Body>
+					<Card.Title as="h1" className="mb-3">
+						Jyväskylän Insinööripalvelu Oy
+					</Card.Title>
+
+					<Card.Text>
+
+					</Card.Text>
+
+					<Card.Text>
+
+					</Card.Text>
+				</Card.Body>
+			</Card>
+		</Container>
 	);
 }
