81dc566c541ca91a16027a95a39808a9db3a1b95
Eetu.E <eetueskelinen123@gmail.com>
2026-04-16 11:07:52 +0300

    Move css to index.css

--- frontend/src/components/Employees/Employee.jsx
+++ frontend/src/components/Employees/Employee.jsx
@@ -5,32 +5,23 @@ export default function Employee({ emp }) {
 	return (
 		<>
 			<Card style={{ width: "20rem" }}>
-				<Card.Img
+				<Card.Img className="employee-card-image"
 					variant="top"
 					src={emp.imageUrl}
-					style={{
-						width: "20rem",
-						height: "13rem",
-						objectFit: "contain",
-						paddingLeft: 1,
-						paddingRight: 3,
-						paddingTop: 1,
-						paddingBottom: 1,
-					}}
 				/>
 				<Card.Body>
 					<Card.Title variant="" style={{fontSize:"25px"}}>{emp.name}</Card.Title>					
 				</Card.Body>
 				<ListGroup className="list-group-flush">
-					<ListGroup.Item style={{ fontSize: 15 }}>{emp.role}</ListGroup.Item>
-					<ListGroup.Item style={{ fontSize: 15 }}>
+					<ListGroup.Item className="card-text-small">{emp.role}</ListGroup.Item>
+					<ListGroup.Item className="card-text-small">
 						{emp.contacts[0].phone}
 					</ListGroup.Item>
-					<ListGroup.Item style={{ fontSize: 15 }}>
+					<ListGroup.Item className="card-text-small">
 						{emp.contacts[0].email}
 					</ListGroup.Item>
 				</ListGroup>
-					<Card.Text style={{fontSize:"20px"}}>{emp.description}</Card.Text>
+					<Card.Text className="card-text-large">{emp.description}</Card.Text>
 			</Card>
 		</>
 	);
